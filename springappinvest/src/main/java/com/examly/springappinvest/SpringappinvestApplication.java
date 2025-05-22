9c3632bbc773f019f429a49bb5a19b2244fde8f4
neojarvis <techadmin@iamneo.ai>
2025-05-22 06:48:36 +0000

    initialCommit-Boilerplate

---
+++ springappinvest/src/main/java/com/examly/springappinvest/SpringappinvestApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springappinvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappinvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappinvestApplication.class, args);
+	}
+
+}
---
+++ springappinvest/src/test/java/com/examly/springappinvest/SpringappinvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examly.springappinvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappinvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
