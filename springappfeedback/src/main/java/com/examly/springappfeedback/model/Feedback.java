4427446d238e48e5e81c86ff32069d3a5f48eb29
neojarvis <techadmin@iamneo.ai>
2025-05-25 12:15:42 +0000

    Manual-save-commit-5/25/2025--5:45:40-PM

--- springappfeedback/src/main/java/com/examly/springappfeedback/model/Feedback.java
+++ springappfeedback/src/main/java/com/examly/springappfeedback/model/Feedback.java
@@ -4,15 +4,24 @@ import java.lang.annotation.Inherited;
 
 import javax.annotation.processing.Generated;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 
+@Entity
 public class Feedback {
     @Id 
     @GeneratedValue(strategy=GeneratedType.IDENTITY)
     private Long feedbackId;
     private String feedbackText;
     private String date;
+    @ManyToOne
+    @JoinColumn(name="userId",nullable = false)
     private User user;
+    @ManyToOne
+    @JoinColumn(name="investmentId",nullable=true)
     private Investment investment;
     private String category;
     
