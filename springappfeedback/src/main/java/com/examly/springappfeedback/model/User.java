67eb5c84109d9fc21c60d10fc1ddb8f387d37487
neojarvis <techadmin@iamneo.ai>
2025-05-25 11:48:43 +0000

    Manual-save-commit-5/25/2025--5:18:24-PM

--- springappfeedback/src/main/java/com/examly/springappfeedback/model/User.java
+++ springappfeedback/src/main/java/com/examly/springappfeedback/model/User.java
@@ -1,3 +1,65 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String email;
+    private String password;
+    private String username;
+    private String mobileNumber;
+    private String userRole;
+    
+    
+    public User() {
+    }
+    public User(Long userId, String email, String password, String username, String mobileNumber, String userRole) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.mobileNumber = mobileNumber;
+        this.userRole = userRole;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    public String getUserRole() {
+        return userRole;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
 
 }
