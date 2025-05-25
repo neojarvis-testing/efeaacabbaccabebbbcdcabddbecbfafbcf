245784cbd597b057ae0499ec6c6c9f5b39a31214
neojarvis <techadmin@iamneo.ai>
2025-05-25 12:05:49 +0000

    Manual-save-commit-5/25/2025--5:35:49-PM

--- springappinvest/src/main/java/com/examly/springappinvest/model/InvestmentInquiry.java
+++ springappinvest/src/main/java/com/examly/springappinvest/model/InvestmentInquiry.java
@@ -1,3 +1,16 @@
+import java.lang.annotation.Inherited;
+
+import javax.annotation.processing.Generated;
+
+@Entity
 public class InvestmentInquiry {
+    @Id 
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long inquiryId;
+private User user;
+@ManyToOne
+@JoinColumn(name="investmentId",nullable=false)
+private Investment investment;
+
 
 }
