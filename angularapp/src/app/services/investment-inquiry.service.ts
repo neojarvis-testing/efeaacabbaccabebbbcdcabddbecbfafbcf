f94914655e0311c333bad771fec9dd13a6ec4057
neojarvis <techadmin@iamneo.ai>
2025-05-26 09:41:48 +0000

    Manual-save-commit-5/26/2025--3:11:47-PM

--- angularapp/src/app/services/investment-inquiry.service.ts
+++ angularapp/src/app/services/investment-inquiry.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 // import { Observable } from 'rxjs/dist/types';
-import { Observable } from 'rxjs/dist/types';
+import { Observable } from 'rxjs';
 import { InvestmentInquiry } from '../models/investment-inquiry.model';
 
 @Injectable({
