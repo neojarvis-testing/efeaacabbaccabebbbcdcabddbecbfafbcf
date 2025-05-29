9a92d5903aeb1413f2e4c2f345df7766d545544b
mukeshvelusamy <75516290+mukeshvelusamy@users.noreply.github.com>
2025-05-29 11:28:34 +0530

    Update AuthController.java

--- springappuser/src/main/java/com/examly/springappuser/controller/AuthController.java
+++ springappuser/src/main/java/com/examly/springappuser/controller/AuthController.java
@@ -1,28 +1,37 @@
 package com.examly.springappuser.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import com.examly.springappuser.model.User;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.examly.springappuser.model.LoginDTO;
+import com.examly.springappuser.model.User;
 import com.examly.springappuser.service.UserService;
 
-import com.examly.springappuser.service.UserServiceImpl;
 
 @RestController
-@RequestMapping("/api")
 public class AuthController {
 
-    @Autowired
-    private UserServiceImpl userService;
+    private final UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody User user){
-        userService.registerUser(user);
-        return new ResponseEntity<>("User registered successfully",HttpStatus.CREATED);
+    public AuthController(UserService userService) {
+        this.userService = userService;
     }
+
+    @PostMapping("/api/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+
+        String userResponse = userService.createUser(user);
+
+        return ResponseEntity.created(URI.create("/api/user"))
+                .body(userResponse);
+    }
+
+    @PostMapping("/api/login")
+    public ResponseEntity<LoginDTO> loginUser(@RequestBody User user) {
+
+        return ResponseEntity.ok(userService.loginUser(user));
+    }
+
 }
