77e22bf00b93baef08ff47f07e993dc1762b00d6
mukeshvelusamy <75516290+mukeshvelusamy@users.noreply.github.com>
2025-05-29 11:33:28 +0530

    Update User.java

--- springappuser/src/main/java/com/examly/springappuser/model/User.java
+++ springappuser/src/main/java/com/examly/springappuser/model/User.java
@@ -1,4 +1,5 @@
 package com.examly.springappuser.model;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -6,19 +7,22 @@ import jakarta.persistence.Id;
 
 @Entity
 public class User {
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+
+    // userid, email, password, username, mobileNumber,userRole
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
     private String email;
     private String password;
     private String username;
     private String mobileNumber;
     private String userRole;
-    
-    
+
     public User() {
     }
-    public User(Long userId, String email, String password, String username, String mobileNumber, String userRole) {
+
+    public User(int userId, String email, String password, String username, String mobileNumber, String userRole) {
         this.userId = userId;
         this.email = email;
         this.password = password;
@@ -26,39 +30,51 @@ public class User {
         this.mobileNumber = mobileNumber;
         this.userRole = userRole;
     }
-    public Long getUserId() {
+
+    public int getUserId() {
         return userId;
     }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
     public String getEmail() {
         return email;
     }
-    public String getPassword() {
-        return password;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-    public String getUserRole() {
-        return userRole;
-    }
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
     public void setMobileNumber(String mobileNumber) {
         this.mobileNumber = mobileNumber;
     }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
     public void setUserRole(String userRole) {
         this.userRole = userRole;
     }
