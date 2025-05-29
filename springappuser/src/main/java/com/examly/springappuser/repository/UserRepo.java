0d0db62029454a61b1e786d9815129110efe2758
mukeshvelusamy <75516290+mukeshvelusamy@users.noreply.github.com>
2025-05-29 11:30:04 +0530

    Update UserRepo.java

--- springappuser/src/main/java/com/examly/springappuser/repository/UserRepo.java
+++ springappuser/src/main/java/com/examly/springappuser/repository/UserRepo.java
@@ -1,11 +1,18 @@
 package com.examly.springappuser.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.examly.springappuser.model.User;
-@Repository
-public interface UserRepo extends JpaRepository<User,Long>{
-  
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 
 }
